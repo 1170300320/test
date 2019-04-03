e8631338c3a55a505b5140a157f0e1e9b0ce7c56
1170300320 <48018373+1170300320@users.noreply.github.com>
2019-04-03 19:45:20 +0800

    lay

---
+++ Lab2-1170300320/src/P2/Person.java
@@ -0,0 +1,17 @@
+package P2;
+
+
+public class Person {
+
+	int num;
+	public String name;
+	public Person(String name) {
+		this.name = name;
+	}
+	/*public int num()
+	{
+	return num;
+	}*/
+	
+
+}
