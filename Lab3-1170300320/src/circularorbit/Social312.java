f0b13c58236e67bf7bc10253e80e2dd82bcf184e
1170300320 <48018373+1170300320@users.noreply.github.com>
2019-05-05 12:51:33 +0800

    k

--- Lab3-1170300320/src/circularorbit/Social312.java
+++
@@ -1,5 +0,0 @@
-package circularorbit;
-
-public class Social312 {
-
-}
--- Lab3-1170300320/src/circularorbit/factory.java
+++
@@ -1,6 +0,0 @@
-package circularorbit;
-//³éÏó¹¤³§
-public interface factory<L,E> {
-    
-	public ConcreteCircularOrbit<L,E> c();
-}
